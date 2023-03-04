066220b2f7f2e32d553312e0d4f8e9f654feaed0
N.Pozdnjakov <N.Pozdnjakov@prosoftsystems.ru>
2023-03-05 01:49:10 +0500

    Add custom view for emoji

--- Homework_2/app/src/main/java/com/tinkoff/homework/MainActivity.kt
+++ Homework_2/app/src/main/java/com/tinkoff/homework/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tinkoff.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tinkoff.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.reactionView.setOnClickListener {
+            it.isSelected = !it.isSelected
+        }
     }
 }
