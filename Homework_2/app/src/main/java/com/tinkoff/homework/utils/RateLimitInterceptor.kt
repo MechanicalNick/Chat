15a9250ab55397a08fad8ae6d394baff46adeee0
N.Pozdnjakov <N.Pozdnjakov@prosoftsystems.ru>
2023-04-18 22:20:25 +0500

    Exponential RateLimitInterceptor

--- Homework_2/app/src/main/java/com/tinkoff/homework/utils/RateLimitInterceptor.kt
+++ Homework_2/app/src/main/java/com/tinkoff/homework/utils/RateLimitInterceptor.kt
@@ -1,22 +1,32 @@
 package com.tinkoff.homework.utils
 
+import android.util.Log
 import okhttp3.Interceptor
 import java.io.IOException
 
 
 class RateLimitInterceptor : Interceptor {
+    private val defaultSleepMiils = 1000L
+    private var sleepMiils = defaultSleepMiils
+
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         var response = chain.proceed(chain.request())
 
-        if (!response.isSuccessful && response.code == 429) {
-            System.err.println(response.message)
-            try {
-                println("wait and retry...")
-                Thread.sleep(1000)
-            } catch (e: InterruptedException) {
+        if (response.isSuccessful) {
+            sleepMiils = defaultSleepMiils
+        } else {
+            if (response.code == 429) {
+                Log.e("rate limit error", response.message)
+                System.err.println()
+                try {
+                    Log.e("rate limit error", "retrying $sleepMiils ms")
+                    Thread.sleep(sleepMiils)
+                    sleepMiils *= 2
+                } catch (e: InterruptedException) {
+                }
+                response = chain.proceed(chain.request())
             }
-            response = chain.proceed(chain.request())
         }
 
         return response
