f4731334cf670fd9c75f0a4a887df210f65c7d36
N.Pozdnjakov <N.Pozdnjakov@prosoftsystems.ru>
2023-04-18 23:51:14 +0500

    Max sleep time

--- Homework_2/app/src/main/java/com/tinkoff/homework/utils/RateLimitInterceptor.kt
+++ Homework_2/app/src/main/java/com/tinkoff/homework/utils/RateLimitInterceptor.kt
@@ -3,11 +3,13 @@ package com.tinkoff.homework.utils
 import android.util.Log
 import okhttp3.Interceptor
 import java.io.IOException
+import java.lang.Long.min
 
 
 class RateLimitInterceptor : Interceptor {
     private val defaultSleepMiils = 1000L
     private var sleepMiils = defaultSleepMiils
+    private val maxSleepMills = 120000L // two minutes
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
@@ -21,7 +23,7 @@ class RateLimitInterceptor : Interceptor {
                 System.err.println()
                 try {
                     Log.e("rate limit error", "retrying $sleepMiils ms")
-                    Thread.sleep(sleepMiils)
+                    Thread.sleep(min(sleepMiils, maxSleepMills))
                     sleepMiils *= 2
                 } catch (_: InterruptedException) {
                 }
