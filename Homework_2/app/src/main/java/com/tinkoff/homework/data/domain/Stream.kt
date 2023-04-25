e597c5d352510fc02e4be466cb707b00c4ce28bd
N.Pozdnjakov <N.Pozdnjakov@prosoftsystems.ru>
2023-04-25 18:06:21 +0500

    Fix crush after parcelable

--- Homework_2/app/src/main/java/com/tinkoff/homework/data/domain/Stream.kt
+++ Homework_2/app/src/main/java/com/tinkoff/homework/data/domain/Stream.kt
@@ -1,8 +1,12 @@
 package com.tinkoff.homework.data.domain
 
-class Stream(
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Stream(
     val id: Long,
     val name: String,
     val topics: MutableList<Topic>,
     var isExpanded: Boolean
-)
+): Parcelable
